e09346ad26212a2435a304ef07f512395fdc4f4a
Isaque Araújo Nogueira <isaquean2@gmail.com>
2020-12-18 13:46:52 -0300

    first commit

---
+++ DellLead_Aula1.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace DellLead_Aula1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much data will you enter? ");
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0,n1;
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Number #" + i + ": ");
+                n1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                sum +=n1;
+            }
+            Console.WriteLine("Sum: " + sum.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
